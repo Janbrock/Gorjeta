92f0bae682aed935af1c66e4b0096e812c92ec18
DadStalin <eujeanlima386@gmail.com>
2021-10-17 13:24:07 -0300

    Bug de crash quando Cost of Service fosse null corrigido

--- app/src/main/java/com/example/gorjeta/MainActivity.kt
+++ app/src/main/java/com/example/gorjeta/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+        val cost = stringInTextField.toDoubleOrNull() ?: return
         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
