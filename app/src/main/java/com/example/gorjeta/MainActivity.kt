958a8e881dd99d75ad4f9f51a0ce1772817cd30a
DadStalin <eujeanlima386@gmail.com>
2021-10-16 23:06:16 -0300

    Layout interage com o usuário e calcula a gorjeta

--- app/src/main/java/com/example/gorjeta/MainActivity.kt
+++ app/src/main/java/com/example/gorjeta/MainActivity.kt
@@ -16,20 +16,20 @@ class MainActivity : AppCompatActivity() {
         binding.calculateButton.setOnClickListener { calculateTip() }
     }
 
-    fun calculateTip(){
+    fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDouble()
-        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId){
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
-        var tip = cost*tipPercentage
+        var tip = cost * tipPercentage
         val roundUp = binding.roundUpSwitch.isChecked
-        if(roundUp){
+        if (roundUp) {
             tip = kotlin.math.ceil(tip)
         }
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text =  getString(R.string.tip_amount, formattedTip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
